ece546ebd38b11f14535d41090e86ab90281106c
graibn <5966878+t1ooo@users.noreply.github.com>
2022-02-17 14:58:36 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'src/app.dart';
 import 'src/init.dart';
 import 'src/prototype.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   configureLogger();
 
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // final NotificationService notificationService = Platform.isAndroid
-  //     ? (AwesomeNotificationService(
-  //         key: 'mutlitimer key',
-  //         name: 'mutlitimer name',
-  //         description: 'mutlitimer desc',
-  //         updateChannel: true,
-  //       )..init())
-  //     : TimerNotificationService();
-
   final firstRun = await FirstRun.init();
-  // await firstRun.reset();
-
   runApp(
-    /// Providers are above [MyApp] instead of inside it, so that tests
-    /// can use [MyApp] while mocking the providers
     MultiRepositoryProvider(
       providers: [
         RepositoryProvider.value(value: Clock()),
-        RepositoryProvider.value(value: await settingsRepo(firstRun.isFirstRun)),
+        RepositoryProvider.value(
+            value: await settingsRepo(firstRun.isFirstRun)),
         RepositoryProvider.value(value: await timerRepo(firstRun.isFirstRun)),
         RepositoryProvider.value(
           value: await notificationService(),
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,17 +5,12 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'l10n/gen/app_localizations.dart';
 import 'prototype.dart';
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
 import 'settings_repository.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({
     Key? key,
-    // required this.settingsController,
   }) : super(key: key);
-
-  // final SettingsController settingsController;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +20,6 @@ class MyApp extends StatelessWidget {
         // TODO: move to main ?
         BlocProvider<TimersCubit>(
           create: (context) =>
-              // TimersCubit(RepositoryProvider.of<TimerRepo>(context))..load(),
               TimersCubit(context.read<TimerRepo>(), context.read<Clock>())
                 ..load(),
         ),
@@ -39,48 +33,18 @@ class MyApp extends StatelessWidget {
   }
 
   Widget builder(BuildContext context) {
-    // final cubit = context.watch<SettingsCubit>();
     return BlocBuilder<SettingsCubit, SettingsCubitState>(
-      // animation: settingsController,
       builder: (BuildContext context, SettingsCubitState state) {
-        // print('rebuild material app: ${state.settings?.locale}');
         return MaterialApp(
           restorationScopeId: 'app',
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
           supportedLocales: AppLocalizations.supportedLocales,
           locale: state.settings?.locale,
-          // localeResolutionCallback:  (Locale? _, Iterable<Locale>__)  {
-
-          // },
-          // localeListResolutionCallback: (List<Locale>? _, Iterable<Locale> __ ) {
-
-          // },
           onGenerateTitle: (BuildContext context) =>
               AppLocalizations.of(context)!.appTitle,
           theme: ThemeData(),
           darkTheme: ThemeData.dark(),
-          // themeMode: settingsController.themeMode,
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                switch (routeSettings.name) {
-                  case SettingsView.routeName:
-                    return SettingsView();
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
-                  default:
-                    return HomeView();
-                }
-              },
-            );
-          },
+          home: HomeView(),
         );
       },
     );
